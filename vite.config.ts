dfaf409f9ba517cf964a37d0c775f98623f672dc
Quresh Antri <qureshantri5152@gmail.com>
2025-08-20 19:47:18 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,14 +5,19 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+
+  base: "/echoverse-walls/", // <-- Add this line back in
+
   server: {
-    host: "::",
+    host: "",
     port: 8080,
   },
   plugins: [
     react(),
-    mode === 'development' &&
-    componentTagger(),
+    mode === "development" &&
+      componentTagger({
+        /* ... */
+      }),
   ].filter(Boolean),
   resolve: {
     alias: {
